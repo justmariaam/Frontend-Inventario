c772fead982148285da84597c054e1a1e7ca449d
justmariaam <mhdez914@gmail.com>
2025-12-11 12:41:57 -0600

    Despliegue

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * URL base para consumir el backend.
  * @constant {string}
  */
-const API_URL = "https://inventario-backend-production-1c13.up.railway.app/api/productos";
+const API_URL = "https://inventario-backend-production-4123.up.railway.app/api/productos";
 
 /**
  * URL base para consumir el desde local.
